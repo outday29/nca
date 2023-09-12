dbd84fa611bd01d45e13dd36182cb11f2923ff2f
engkheng <ongengkheng929@gmail.com>
2023-09-12 22:11:29 +0800

    update

--- download_dataset.py
+++ download_dataset.py
@@ -3,12 +3,16 @@ from zipfile import ZipFile
 from pathlib import Path
 import os
 
-zipresp = urlopen("http://kvfrans.com/static/32_emoji.zip")
-tempzip = open("temp.zip", "wb")
-tempzip.write(zipresp.read())
-tempzip.close()
-zf = ZipFile("temp.zip")
-zf.extractall(path = Path("./pic"))
-zf.close()
+def download_dataset(output_dir):
+    zipresp = urlopen("http://kvfrans.com/static/32_emoji.zip")
+    tempzip = open("temp.zip", "wb")
+    tempzip.write(zipresp.read())
+    tempzip.close()
+    zf = ZipFile("temp.zip")
+    zf.extractall(path = output_dir)
+    zf.close()
+    
+    os.remove("temp.zip")
 
-os.remove("temp.zip")
+if __name__ == "__main__":
+    download_dataset(Path("./pic"))
